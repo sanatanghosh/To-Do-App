3d543143e814a3c7632e9ac60f6ec081e59e9107
sanatanghosh <sanatanghosh1997@gmail.com>
2020-04-18 20:01:49 +0530

    Updates

---
+++ precache-manifest.bf3709bd4c533806eef5736cc007045b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ca663cadafcf0179ad1d95fc96dc0cc",
+    "url": "/to-do-app/index.html"
+  },
+  {
+    "revision": "af0899d6b40f2ee63393",
+    "url": "/to-do-app/static/css/main.cc4d4430.chunk.css"
+  },
+  {
+    "revision": "fab28ea9dcfc0f1778f4",
+    "url": "/to-do-app/static/js/2.0e1dfdfe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to-do-app/static/js/2.0e1dfdfe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af0899d6b40f2ee63393",
+    "url": "/to-do-app/static/js/main.b3ee44c5.chunk.js"
+  },
+  {
+    "revision": "cabf27c304aa1808fd32",
+    "url": "/to-do-app/static/js/runtime-main.39ce08d8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/to-do-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
